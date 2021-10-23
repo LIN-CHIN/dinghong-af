a049eccbf023087663a22497b70b7e39f9737419
chin <s235068267>
2021-10-23 12:51:46 +0800

    [新增]公司設立前二頁面，[修改] Header 用js帶出

---
+++ js/addHeader.js
@@ -0,0 +1,115 @@
+    $("header").append(`
+    <!-- header-area start -->
+    <div id="sticker" class="header-area hidden-xs">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <div class="row">
+                        <!-- logo -->
+                        <div class="col-md-3 col-sm-3">
+                            <div class="logo">
+                                <a class="navbar-brand page-scroll white-logo" href="index-2.html">
+                                    <img src="img/logo/dhlogo.png" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <!-- logo end -->
+                        <div class="col-md-9 col-sm-9">
+                            <!-- Navibar start -->
+                            <nav class="navbar navbar-default">
+                                <div class="collapse navbar-collapse" id="navbar-example">
+                                    <div class="main-menu">
+                                        <ul class="nav navbar-nav navbar-right">
+                                            <li><a href="index.html">Home</a></li>
+                                            <li><a href="about.html">About us</a></li>
+                                            <li><a class="pages" href="#">公司設立</a>
+                                                <ul class="sub-menu">
+                                                    <li><a href="establish-item.html">公司登記項目</a></li>
+                                                    <li><a href="establish-process.html">公司設立流程</a></li>
+                                                    <li><a href="establish-company-difference.html">公司與行號差異</a></li>
+                                                    <li><a href="establish-lock-company.html">閉鎖性公司設立</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a class="pages" href="index-2.html">帳務與稅務</a>
+                                                <ul class="sub-menu">
+                                                    <li><a href="index-2.html">帳務服務內容</a></li>
+                                                    <li><a href="index-3.html">報稅時程</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a class="pages" href="index-2.html">會計師簽證</a>
+                                                <ul class="sub-menu">
+                                                    <li><a href="index-2.html">稅務簽證</a></li>
+                                                    <li><a href="index-3.html">財務簽證</a></li>
+                                                    <li><a href="index-3.html">資本簽證</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a class="pages" href="index-2.html">服務收費標準</a>
+                                                <ul class="sub-menu">
+                                                    <li><a href="index-2.html">服務收費標準</a></li>
+                                                    <li><a href="index-3.html">公司設立~EDM</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+                            <!-- Navibar end -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- header-area end -->
+    <!-- mobile-menu-area start -->
+    <div class="mobile-menu-area hidden-lg hidden-md hidden-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mobile-menu">
+                        <!-- logo Start-->
+                        <div class="logo">
+                            <a href="index.html"><img src="img/logo/dhlogo.png" alt="" /></a>
+                        </div>
+                        <!-- logo End-->
+                        <nav id="dropdown">
+                            <ul>
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="about.html">About us</a></li>
+                                <li><a class="pages" href="#">公司設立</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="establish-item.html">公司登記項目</a></li>
+                                        <li><a href="establish-process.html">公司設立流程</a></li>
+                                        <li><a href="establish-company-difference.html">公司與行號差異</a></li>
+                                        <li><a href="establish-lock-company.html">閉鎖性公司設立</a></li>
+                                    </ul>
+                                </li>
+                                <li><a class="pages" href="index-2.html">帳務與稅務</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="index-2.html">帳務服務內容</a></li>
+                                        <li><a href="index-3.html">報稅時程</a></li>
+                                    </ul>
+                                </li>
+                                <li><a class="pages" href="index-2.html">會計師簽證</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="index-2.html">稅務簽證</a></li>
+                                        <li><a href="index-3.html">財務簽證</a></li>
+                                        <li><a href="index-3.html">資本簽證</a></li>
+                                    </ul>
+                                </li>
+                                <li><a class="pages" href="index-2.html">服務收費標準</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="index-2.html">服務收費標準</a></li>
+                                        <li><a href="index-3.html">公司設立~EDM</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- mobile-menu-area end -->`);
+
+
